c12050c0bd644d42b5e9b638d5957b3dbf4c10a3
10933991 <10933991@INFO1-25>
2025-05-17 08:51:45 +0200

    CommitIniziale

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace CampoMinato2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pippo");
+        }
     }
 }
