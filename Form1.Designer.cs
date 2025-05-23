8e2fed3521fe4ea0148c2ec0b0a86e76c26210c4
TuPadre <biolomicrosort@gmail.com>
2025-05-23 15:56:20 +0200

    Scroller Audio per Musica e pulsanti funzionante e aggiunto, + logo pagina iniziale

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btn_Impostazioni = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // btn_Impostazioni
+            // 
+            btn_Impostazioni.Location = new Point(778, 378);
+            btn_Impostazioni.Name = "btn_Impostazioni";
+            btn_Impostazioni.Size = new Size(94, 29);
+            btn_Impostazioni.TabIndex = 0;
+            btn_Impostazioni.Text = "button1";
+            btn_Impostazioni.UseVisualStyleBackColor = true;
+            btn_Impostazioni.Click += btn_Impostazioni_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources._4649c4a3_3e0c_4db7_8abe_8205bed2de2f_removebg_preview;
+            pictureBox1.Location = new Point(243, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(429, 251);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(914, 600);
+            Controls.Add(pictureBox1);
+            Controls.Add(btn_Impostazioni);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btn_Impostazioni;
+        private PictureBox pictureBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,46 @@
 namespace CampoMinato2
 {
+    using System;
+    using NAudio.Wave;
     public partial class Form1 : Form
     {
+
+        FImpostazioni impostazioni = new FImpostazioni();
+        
+        
+
+
         public Form1()
         {
             InitializeComponent();
+            Inizializzazioni();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Pippo");
+
+        }
+
+        private void btn_Impostazioni_Click(object sender, EventArgs e)
+        {
+            //prendo l'istanza del suono dal form impostazioni
+            
+            impostazioni.pulsantePremuto();
+
+            impostazioni.ShowDialog();
+
+            
+        }
+
+        public void Inizializzazioni()
+        {
+            impostazioni.Show();
+            impostazioni.Hide();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
